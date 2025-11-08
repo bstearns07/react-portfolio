644eb531e8614edc9cc0977b2a689c9c3ec72534
bstearns07 <bstearns07@gmail.com>
2025-11-07 23:09:49 -0600

    added amplify

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
