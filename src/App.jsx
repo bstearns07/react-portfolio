dc374df54d8226725333e10368f6841874c9574b
bstearns07 <bstearns07@gmail.com>
2025-11-09 23:55:31 -0600

    lowercased

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header.jsxs";
+import Header from "./components/header.jsxs";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About";
 import Projects from "./components/Projects";
