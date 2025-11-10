74a18c9057c8c5d63a6e9a43c840347fe5769b36
bstearns07 <bstearns07@gmail.com>
2025-11-09 23:59:33 -0600

    change back to uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header.jsxs";
+import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About";
 import Projects from "./components/Projects";
